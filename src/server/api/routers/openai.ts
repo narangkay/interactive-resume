1c9df9b0b12f90e5b61a7de6f3606cc99c9aee9b
Krish <kanarang@gmail.com>
2023-05-23 20:07:18 -0700

    cleanup openai fetch

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -4,79 +4,44 @@ import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { TRPCError } from '@trpc/server';
 
 import { env } from "~/env.mjs";
+import { askAboutResumePrompt, followupQuestionsPrompt } from "~/server/api/utils/prompts";
 
-const RESUME = `Google — Senior Software Engineer
-Subscriber Acquisition And Management Platform, Team Lead
-2022-Present
-- Leading a 14 engineer, full-stack team responsible for XXXM$ in new revenue for Google every year (XXM paid subscribers)
-- Doubled our subscriber base over the past year by developing cross-platform, turnkey solutions for subscriber acquisition (embeddable UI components, SDKs, 1P APIs) and integrating with all major Google products (GMail, Photos, Search, Drive etc)
-- Supported a wide range of subscription management capabilities, including cross-platform billing, powerful user targeting and analytics, lifecycle management, and provisioning of benefits.
-Anomaly Detection And Explainability Of Non-Deterministic Systems
-2019-2021
-- Google owns multiple non-deterministic systems like ranking and NLP that fail in unpredictable ways, our team was tasked with solving reliability and quality issues of these systems at scale
-- Used model distillation techniques to capture complex system behavior into simple, explainable student models. These are then run alongside the original system in production to detect drastic changes in behavior, which is often a regression indicator
-- Successfully demonstrated the value of this technique, and subsequently scaled our model serving infra to millions of QPS to deploy models across Google Search, Assistant, and GMail
-Moderation Of User Generated Content On Google Maps
-2017-2019
-- Developed and launched human-in-the-loop workflows that use outputs from existing ML models to create targeted tasks for manual review, and feed the task outputs back as training data
-- Delivered 55% reduction in manual review cost due to targeted nature of the tasks, and a 6% increase in published user content
-Research Projects
-- Automated Ranking And Classification Of Musician Skill. Research Thesis with Prof Preeti Rao, Aug 2016 - May 2017
-- Battery Life And Charge Time Optimization For Li-ion Batteries. Samsung R&D, May 2016 - Jul 2016
-- Wearable Devices For Distributed Audio Experiences. SoundRex, Jan 2016 - Apr 2016
-PUBLICATIONS AND AWARDS
-- Patent US11568869 [2023-01-31]. Low latency automated identification of automated assistant function failure.
-- Thesis Publication, ISMIR 2017. Acoustic features for determining goodness of tabla strokes.
-SKILLS AND TECHNOLOGIES
-Systems and Architecture Design
-- Microservices (gRPC - Java/ C++/ Go)
-- Event-driven (Pub/Sub, Cron)
-- SaaS (SDKs, Webviews, APIs)
-- Database Management (Spanner)
-- Pipelines (Flume, MapReduce)
-- Optimization (Memcached, Fibers)
-- Monitoring/Alerting (Proprietary)
-Machine Learning and AI
-- Training (Custom Stack, Tensorflow)
-- Feature Engineering (NumPy, Beam)
-- Deployment (Custom Stack, TFX)
-- On-device (Google ML Kit)
-Frontend Development
-- Web (Closure, React) 
-- Mobile (Flutter, Android Native)
-Agility & Versatility: Tackling problems holistically through expertise in multiple domains, for example - Google’s subscription platform
-Novel Solutions: Applying concepts across disciplines in unique ways, using model distillation  to measure reliability of distributed systems
-Collaboration: Leading and growing teams with a focus on sustainable culture and transparency`
-
-const PREFIX = "You are Krish, a Senior Software Engineer at Google. You have studied engineering at the Indian Institute of Bombay. You are looking for a new job and have been contacted by a recruiter with questions about your resume. Your resume is provided below. Do not make up any information that is not in the resume."
-
-const PROMPT_INSTRUCTIONS = `${PREFIX}
-Do your best to answer the questions and get the job. For example:
-What is your current role?
-I am a Senior Software Engineer at Google, leading a team of 14 engineers responsible for Google's subscriber acquisition and management platform.
-
-What was your longest role?
-I have been at Google for 5 years, and have been a Senior Software Engineer for 3 years.
-
-Q: What do you know about distributed systems?
-A: I have worked on all aspects of distributed systems at Google, including microservices, event-driven systems, reliability and observability, latency optimization, database management.
-
-Your resume for reference:
-${RESUME}
-`
-
-const FOLLOWUP_QUESTIONS_PROMPT = `${PREFIX}
-Suggest at least 3 followup questions that you think the recruiter might ask from a variety of areas. Respond with only the questions, each on a different line. Limit your questions to things covered in the resume. For example:
-The recruiter has asked the following question: What do you know about distributed systems?
-Followup questions:
-What areas of distributed systems have you worked on?
-What is your favorite part of working at Google?
-What are areas where you can improve?
-
-Your resume for reference:
-${RESUME}
-
-`
+const openAiFetch = async <T>(url: string, params: any): Promise<T> => {
+  console.log(params)
+  console.log(`env.OPENAI_API_KEY: ${env.OPENAI_API_KEY}`)
+  const fetchResponse = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify(params),
+  })
+  console.log(fetchResponse.status)
+  console.log(fetchResponse.statusText)
+  if (fetchResponse.status !== 200) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: `OpenAI returned error ${fetchResponse.status}: ${fetchResponse.statusText}`,
+    });
+  }
+  type JSONResponse = {
+    id: string,
+    choices: Array<T>,
+  }
+  const data = await fetchResponse.json() as JSONResponse
+  console.log(data)
+  console.log(data?.id)
+  const response = data?.choices[0]
+  console.log(response)
+  if (!response) {
+    throw new TRPCError({
+      code: 'INTERNAL_SERVER_ERROR',
+      message: 'OpenAI returned no response',
+    });
+  }
+  return response
+}
 
 export const openaiRouter = createTRPCRouter({
   askAboutResume: publicProcedure
@@ -92,44 +57,14 @@ export const openaiRouter = createTRPCRouter({
     )
     .output(z.object({ response: z.string() }))
     .mutation(async ({ input: { messages } }) => {
-      messages = [{ role: "assistant", content: PROMPT_INSTRUCTIONS }, ...messages]
-      console.log(PROMPT_INSTRUCTIONS)
+      messages = [{ role: "assistant", content: askAboutResumePrompt() }, ...messages]
       console.log(messages[messages.length - 1])
-      const fetchResponse = await fetch("https://api.openai.com/v1/chat/completions", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${env.OPENAI_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          model: "gpt-3.5-turbo",
-          temperature: 0,
-          messages: messages,
-        }),
+      const response = await openAiFetch<{ message: { content: string } }>("https://api.openai.com/v1/chat/completions", {
+        model: "gpt-3.5-turbo",
+        temperature: 0,
+        messages: messages,
       })
-      console.log(fetchResponse.status)
-      console.log(fetchResponse.statusText)
-      if (fetchResponse.status !== 200) {
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: `OpenAI returned error ${fetchResponse.status}: ${fetchResponse.statusText}`,
-        });
-      }
-      type JSONResponse = {
-        id: string,
-        choices: Array<{ message: { content: string } }>,
-      }
-      const data = await fetchResponse.json() as JSONResponse
-      console.log(data)
-      console.log(data?.id)
-      console.log(data?.choices[0])
-      const answer = data?.choices[0]?.message?.content;
-      if (!answer) {
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'OpenAI returned no response',
-        });
-      }
+      const answer = response.message.content;
       return {
         response: answer
       };
@@ -138,44 +73,14 @@ export const openaiRouter = createTRPCRouter({
     .input(z.object({ lastQuestion: z.string(), }))
     .output(z.object({ followupQuestions: z.array(z.string()) }))
     .mutation(async ({ input: { lastQuestion } }) => {
-      const prompt = `${FOLLOWUP_QUESTIONS_PROMPT}The recruiter has asked the following question: ${lastQuestion}
-Followup questions:`
-      const fetchResponse = await fetch("https://api.openai.com/v1/completions", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${env.OPENAI_API_KEY}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          model: "text-curie-001",
-          temperature: 0,
-          prompt: prompt,
-          max_tokens: 200,
-        }),
+      const prompt = followupQuestionsPrompt(lastQuestion)
+      const response = await openAiFetch<{ text: string }>("https://api.openai.com/v1/completions", {
+        model: "text-curie-001",
+        temperature: 0,
+        prompt: prompt,
+        max_tokens: 200,
       })
-      console.log(fetchResponse.status)
-      console.log(fetchResponse.statusText)
-      if (fetchResponse.status !== 200) {
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: `OpenAI returned error ${fetchResponse.status}: ${fetchResponse.statusText}`,
-        });
-      }
-      type JSONResponse = {
-        id: string,
-        choices: Array<{ text: string }>,
-      }
-      const data = await fetchResponse.json() as JSONResponse
-      console.log(data)
-      console.log(data?.id)
-      console.log(data?.choices[0])
-      const answer = data?.choices[0]?.text;
-      if (!answer) {
-        throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
-          message: 'OpenAI returned no response',
-        });
-      }
+      const answer = response.text;
       const followupQuestions = answer.split('\n').map((q) => q.trim()).filter((q) => q.length > 5).filter((q) => q !== 'Followup questions:')
       return { followupQuestions }
     }),
