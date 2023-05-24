cfcf8aa32f78879bee621fe413b495e68ba1b701
Krish <kanarang@gmail.com>
2023-05-24 01:27:12 -0700

    final polyfill update

--- src/server/api/routers/openai.ts
+++ src/server/api/routers/openai.ts
@@ -1,33 +1,19 @@
 import { TRPCError } from "@trpc/server";
+import { parse } from "path";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-import { askAboutResumeFetch, suggestFollowupQuestionsFetch } from "~/server/api/utils/openaifetch";
+import { askAboutResumeFetch, suggestFollowupQuestionsFetch, parseAskAboutResumeResponse, parseSuggestFollowupQuestionsResponse } from "~/server/api/utils/openaifetch";
 
-const throwErrorOrTransform = async <T>(fetchResponse: Response): Promise<T> => {
+const throwErrorOrTransform = async (fetchResponse: Response, parseResponse: (response: Response) => string): Promise<string> => {
   if (fetchResponse.status !== 200) {
     throw new TRPCError({
       code: 'INTERNAL_SERVER_ERROR',
       message: `OpenAI returned error ${fetchResponse.status}: ${fetchResponse.statusText}`,
     });
   }
-  type JSONResponse = {
-    id: string,
-    choices: Array<T>,
-  }
-  const data = await fetchResponse.json() as JSONResponse
-  console.log(data)
-  console.log(data?.id)
-  const response = data?.choices[0]
-  console.log(response)
-  if (!response) {
-    throw new TRPCError({
-      code: 'INTERNAL_SERVER_ERROR',
-      message: "OpenAI returned no response"
-    })
-  }
-  return response
+  return await fetchResponse.json().then(parseResponse)
 }
 
 
@@ -52,8 +38,7 @@ export const openaiRouter = createTRPCRouter({
       ),
     }))
     .mutation(async ({ input }) => {
-      const response = await askAboutResumeFetch(input).then(throwErrorOrTransform<{ message: { content: string } }>)
-      const answer = response.message.content;
+      const answer = await askAboutResumeFetch(input).then((fetchResponse) => throwErrorOrTransform(fetchResponse, parseAskAboutResumeResponse))
       return {
         response: [
           ...input.messages,
@@ -65,8 +50,7 @@ export const openaiRouter = createTRPCRouter({
     .input(z.object({ lastQuestion: z.string(), }))
     .output(z.object({ response: z.array(z.string()) }))
     .mutation(async ({ input }) => {
-      const response = await suggestFollowupQuestionsFetch(input).then(throwErrorOrTransform<{ text: string }>)
-      const answer = response.text;
+      const answer = await suggestFollowupQuestionsFetch(input).then((fetchResponse) => throwErrorOrTransform(fetchResponse, parseSuggestFollowupQuestionsResponse))
       const followupQuestions = answer.split('\n').map((q) => q.trim()).filter((q) => q.length > 5).filter((q) => q !== 'Followup questions:')
       return { response: followupQuestions }
     }),
--- src/server/api/utils/openaifetch.ts
+++ src/server/api/utils/openaifetch.ts
@@ -17,22 +17,40 @@ const openAiFetch = async (url: string, params: any): Promise<Response> => {
     return fetchResponse
 }
 
-export const askAboutResumeFetch = async (input: askAboutResumeInputType) => {
+export const askAboutResumeFetch = async (input: askAboutResumeInputType, stream: boolean = false) => {
     const messagesWithInstructions = [{ role: "assistant", content: askAboutResumePrompt() }, ...input.messages]
     console.log(messagesWithInstructions[messagesWithInstructions.length - 1])
     return openAiFetch("https://api.openai.com/v1/chat/completions", {
         model: "gpt-3.5-turbo",
         temperature: 0,
         messages: messagesWithInstructions,
+        stream: stream,
     })
 }
 
-export const suggestFollowupQuestionsFetch = async (input: suggestFollowupQuestionsInputType) => {
+export const suggestFollowupQuestionsFetch = async (input: suggestFollowupQuestionsInputType, stream: boolean = false) => {
     const prompt = followupQuestionsPrompt(input.lastQuestion)
     return openAiFetch("https://api.openai.com/v1/completions", {
         model: "text-curie-001",
         temperature: 0,
         prompt: prompt,
         max_tokens: 200,
+        stream: stream,
     })
+}
+
+const parseResponse = <T>(json: any): T | undefined => {
+    const data = json as { id: string, choices: Array<T>, }
+    console.log(data?.id)
+    const response = data?.choices[0]
+    console.log(response)
+    return response
+}
+
+export const parseAskAboutResumeResponse = (json: any): string => {
+    return parseResponse<{ message: { content: string } }>(json)?.message?.content ?? ""
+}
+
+export const parseSuggestFollowupQuestionsResponse = (json: any): string => {
+    return parseResponse<{ text: string }>(json)?.text ?? ""
 }
