8679a720a69c0adf51011d0d5961105373051e5e
narangkay <kanarang@gmail.com>
2023-06-13 20:03:51 -0700

    update to use tvmjs package and not local tvmjs

--- site/src/utils/localmodel.ts
+++ site/src/utils/localmodel.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "react-query";
-import { ChatModule } from "web-llm";
+import { ChatModule } from "web-llm-next";
 import { askAboutResumePrompt } from "~/utils/prompts";
 
 export function useLocalModel(enabled: boolean, progressCallback: (status: string, progress: number) => void) {
