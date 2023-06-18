c18c628b222709ebfb66e75cd363140328fa822c
narangkay <kanarang@gmail.com>
2023-06-18 04:04:10 -0700

    add vicuna weights

--- site/src/utils/localapi.ts
+++ site/src/utils/localapi.ts
@@ -40,6 +40,7 @@ export function useLocalResumeExpert(): resumeExpertType {
             }) => {
             setAskAboutResumeStatus("loading");
             setSuggestFollowupQuestionsStatus("loading");
+            model.data?.resetChat();
             model.data?.generate(input.askAboutResumeInput.lastQuestion, (_step: number, message: string) => {
                 params.onAskAboutResumeSuccess({ response: [...input.askAboutResumeInput.messages, { role: "assistant", content: message }] });
             }).then((response) => {
